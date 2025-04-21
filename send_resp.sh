e592f8edf87efe78666a4621a6cd14d1d3b840da
LiceagaN <nliceagar@gmail.com>
2025-04-20 19:59:56 -0600

    feat: INCR command implemented

--- send_resp.sh
+++ send_resp.sh
@@ -6,9 +6,7 @@ PORT=6379
 
 # Comando RESP que quieras enviar
 
-COMMAND="*3\r\n\$3\r\nSET\r\n\$3\r\nfoo\r\n\$3\r\nbar\r\n"
-echo -e "$COMMAND" | nc $HOST $PORT
-COMMAND="*3\r\n\$6\r\nEXPIRE\r\n\$3\r\nfoo\r\n\$1\r\n5\r\n"
-echo -e "$COMMAND" | nc $HOST $PORT
-COMMAND="*2\r\n\$3\r\nTTL\r\n\$3\r\nfoo\r\n"
+COMMAND="*2\r\n\$4\r\nINCR\r\n\$3\r\nfoo\r\n"
+
+# Enviar comando al servidor y mostrar la respuesta
 echo -e "$COMMAND" | nc $HOST $PORT
